d214828112c1d7e78d84152e1a73b07889f3c0dd
SKALEZ-A <94375178+SKALEZ-A@users.noreply.github.com>
2023-05-12 23:43:56 +0100

    initial

--- multisig.sol
+++ multisig.sol
@@ -2,5 +2,16 @@
 pragma solidity ^0.8.0;
 
 contract Multisig {
-    
+    event Deposit(address indexed sender);
+    event Submit(uint indexed txId);
+    event Approve(address indexed owner, uint indexed txId);
+    event Revoke(address indexed owner, uint indexed txId);
+    event Execute(uint indexed txId);
+
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+    }
 }
