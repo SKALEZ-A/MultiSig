ae4f8dd75be1c7eac7227730a580c64d63219654
SKALEZ-A <94375178+SKALEZ-A@users.noreply.github.com>
2023-05-13 23:56:41 +0100

    initial

--- multisig.sol
+++ multisig.sol
@@ -14,4 +14,43 @@ contract Multisig {
         bytes data;
         bool executed;
     }
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public required;
+
+    Transaction[] public transactions;
+    mapping(uint => mapping(address => bool)) public approved;
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner" );
+        _;
+    }
+
+    // to confirm that the transaction exists before it can be reverted 
+    modifier txExists(uint _txId) {
+        require(_txId < transactions.length, "tx does not exists");
+        _;
+    }
+
+    // to confirm that the transaxtion has not been approved
+    modifier notApproved(uint _txId) {
+        require(!approved[_txId][msg.sender], "tx already approved");
+        _;
+    }
+
+    // to confirm that the transaction has not been executed 
+    modifier notExecuted(uint _txId) {
+        require(!transactions[_txId].executed, "tx already executed");
+        _;
+    }
+
+    constructor (address[] memory _owners, uint _required) {
+        require(_owners.length > 0, "owners required");
+        require(_required > 0 && _required <= _owners.length, "invalid required number of owners");
+
+        for(uint i; i < _owners.length; i++ ) {
+            address owner = _owners[i];
+        }
+    }
 }
