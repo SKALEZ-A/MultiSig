290909a4662d4dd67834a1240613405a98508089
SKALEZ-A <94375178+SKALEZ-A@users.noreply.github.com>
2023-05-15 22:46:09 +0100

    initial

--- multisig.sol
+++ multisig.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract Multisig {
-    event Deposit(address indexed sender);
+    event Deposit(address indexed sender, uint indexed amount);
     event Submit(uint indexed txId);
     event Approve(address indexed owner, uint indexed txId);
     event Revoke(address indexed owner, uint indexed txId);
@@ -51,6 +51,54 @@ contract Multisig {
 
         for(uint i; i < _owners.length; i++ ) {
             address owner = _owners[i];
+
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner is not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+
+        }
+        required = _required;
+    }
+
+    receive() external payable  {
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function submit(address _to, uint _value, bytes calldata _data) external onlyOwner{
+        transactions.push(Transaction({
+            to: _to,
+            value: _value,
+            data: _data,
+            executed: false
+        }));
+        emit Submit(transactions.length - 1);
+    }
+
+    function approve(uint _txId) external onlyOwner notApproved(_txId) notExecuted(_txId) txExists(_txId) {
+        approved[_txId][msg.sender] = true;
+        emit Approve(msg.sender, _txId);
+    }
+
+    function _getApprovalCount(uint _txId) private view returns (uint count) {
+        for(uint i; i < owners.length; i++) {
+            if(approved[_txId][owners[i]]) {
+                count += 1;
+            }
         }
     }
+
+    function execute(uint _txId) external txExists(_txId) notExecuted(_txId) {
+        require(_getApprovalCount(_txId) >= required, "approvals is less than required");
+        Transaction storage transaction = transactions[_txId];
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.to.call{value: transaction.value} (transaction.data);
+        require(success, "tx failed");
+
+        emit Execute(_txId);
+    }
+
 }
